ac3f2bf79b8b846cf8654a3811cceab9365eb77e
patel-purvak <hello2purvak@gmail.com>
2024-07-04 12:57:27 -0400

    Added Video view for Video Player functionality.

---
+++ app/src/main/java/com/purvak/videoplayer/VideoViewActivity.kt
@@ -0,0 +1,38 @@
+package com.purvak.videoplayer
+
+import android.net.Uri
+import android.net.Uri.*
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class VideoViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_video_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        //Find the VideoView class by its id
+        val videoView = findViewById<VideoView>(R.id.testView)
+        //Creating MediaController
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //specify the location of media file
+        val uri: Uri = parse("android.resource://" + packageName + "/raw/test")
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
